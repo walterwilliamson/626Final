49c803800694d4b5ebd1abcb99b8ee1f9025e68a
Jacob Vidergar <jacobvid@umich.edu>
2024-03-18 14:23:28 -0400

    got rid of stuff that didnt work

--- visualize_image.py
+++ visualize_image.py
@@ -7,27 +7,27 @@ import imageio
 
 # ---------- specify these args ----------
 img_id = '1'
-path_img = f'images/{img_id}.tiff' # your path of the image of choice
-path_protein_names = 'protein_names.csv' # your path of protein_names.csv
-n_proteins = 52 # number of proteins 
+path_img = f'images/{img_id}.tiff'  # your path of the image of choice
+path_protein_names = 'protein_names.csv'  # your path of protein_names.csv
+n_proteins = 52  # number of proteins
 # ----------------------------------------
 
-img = imageio.v2.imread(path_img) # read the image
-pts = pd.read_csv(path_protein_names) # read the protein names
+img = imageio.v2.imread(path_img)  # read the image
+pts = pd.read_csv(path_protein_names)  # read the protein names
 
 nc = 5
-nr = int(np.ceil(n_proteins/nc))
+nr = int(np.ceil(n_proteins / nc))
 ss = 3
-fig = plt.figure(figsize=(nc*ss, nr*ss), dpi=150)
+fig = plt.figure(figsize=(nc * ss, nr * ss), dpi=150)
 gs = fig.add_gridspec(nr, nc,
-                      width_ratios=[1]*nc,
-                      height_ratios=[1]*nr)
+                      width_ratios=[1] * nc,
+                      height_ratios=[1] * nr)
 gs.update(wspace=0.5, hspace=0.3)
 
 for i in range(n_proteins):
-    ax = plt.subplot(gs[i//nc, i%nc])
-    im = img[i,:,:]
-    ax.imshow(im, cmap = 'Greys', vmin=np.quantile(im, 0.05), vmax=np.quantile(im, 0.95))
+    ax = plt.subplot(gs[i // nc, i % nc])
+    im = img[i, :, :]
+    ax.imshow(im, cmap='Greys', vmin=np.quantile(im, 0.05), vmax=np.quantile(im, 0.95))
     ax.set_title(f'{pts["Metal Tag"][i]}\n{pts["Target"][i]}')
     ax.set_aspect('equal', adjustable='box')
     # ax.axis('off')

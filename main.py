38beae160abce393feeadc936809cb3b3382094b
Jacob Vidergar <jacobvid@umich.edu>
2024-03-25 15:13:02 -0400

    added more hidden layers and cleaned up comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense, Input
 from data_preprocessing import preprocess_data
+from keras.layers import Dropout
 
 # use function from data_preprocessing.py
 data = preprocess_data()
@@ -21,9 +22,13 @@ model = Sequential()
 # Defining the Input layer
 model.add(Input(shape=(52,)))
 
-# model.add(Dense(units=36, input_dim=52, kernel_initializer='normal', activation='relu'))
+# Add more layers
 model.add(Dense(units=36, activation='relu'))
-# model.add(Dense(units=36, activation='tanh'))
+model.add(Dropout(0.01))
+model.add(Dense(units=24, activation='relu'))
+model.add(Dropout(0.01))
+model.add(Dense(units=12, activation='relu'))
+model.add(Dropout(0.01))
 
 # Output layer
 model.add(Dense(1, kernel_initializer='normal'))
@@ -32,21 +37,21 @@ model.add(Dense(1, kernel_initializer='normal'))
 model.compile(loss='mean_squared_error', optimizer='adam')
 
 # Fit the model to training set
-model.fit(train_X, train_Y, batch_size=5, epochs=300)
+model.fit(train_X, train_Y, batch_size=10, epochs=700)
 
 # Generating Predictions on testing data - drop id column when passing test_X
 # Also not 100% sure this is what we were supposed to past just adding a reminder to
 # ask tomorrow haha
-Predictions = model.predict(test_X.drop(test_X.columns[0], axis=1))
+#Predictions = model.predict(test_X.drop(test_X.columns[0], axis=1))
 
 # For testing
-# Predictions = model.predict(train_X)
-# TestingData = pd.DataFrame(data=train_X, columns=train_X.columns[1:])
+Predictions = model.predict(train_X)
+TestingData = pd.DataFrame(data=train_X, columns=train_X.columns[1:])
 
 # Get test data
 
 # For Wednesday - We don't know the actual outcomes do we? Just realized that we might
 # not need that column. No idea why it's the same number for all of the predictions
-TestingData = pd.DataFrame(data=test_X.drop(test_X.columns[0], axis=1), columns=test_X.columns[1:])
+#TestingData = pd.DataFrame(data=test_X.drop(test_X.columns[0], axis=1), columns=test_X.columns[1:])
 TestingData['Predicted Survival Months'] = Predictions
 print(TestingData.head())
